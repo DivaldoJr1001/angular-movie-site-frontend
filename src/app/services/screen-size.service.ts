17cd02b79408391f422eddebd09aa8db15dadd55
Divaldo Aderaldo de Oliveira Júnior <divaldojr9@gmail.com>
2024-08-29 15:18:24 -0300

    Updated Screen Size Service

--- src/app/services/screen-size.service.ts
+++ src/app/services/screen-size.service.ts
@@ -8,11 +8,22 @@ export class ScreenSizeService {
   private screenWidth$: BehaviorSubject<number> = new BehaviorSubject<number>(0);
   private resizeObserver!: ResizeObserver;
 
-  init(): void {
+  private headerHeight$: BehaviorSubject<number> = new BehaviorSubject<number>(0);
+  private footerHeight$: BehaviorSubject<number> = new BehaviorSubject<number>(0);
+
+  init(options: { headerHeight?: number, footerHeight?: number} = {}): void {
     this.screenWidth$.next(window.innerWidth);
 
+    if (options && options.headerHeight) {
+      this.headerHeight$.next(options.headerHeight);
+    }
+
+    if (options && options.footerHeight) {
+      this.footerHeight$.next(options.footerHeight);
+    }
+
     this.resizeObserver = new ResizeObserver(entries => {
-      for (let entry of entries) {
+      for (const entry of entries) {
         this.screenWidth$.next(entry.contentRect.width);
       }
     });
@@ -21,5 +32,25 @@ export class ScreenSizeService {
 
   getScreenWidthObservable(): BehaviorSubject<number> {
     return this.screenWidth$;
+  }
+
+  getHeaderHeightObservable(): BehaviorSubject<number> {
+    return this.headerHeight$;
+  }
+
+  setHeaderHeight(height: number): void {
+    this.headerHeight$.next(height);
+  }
+
+  getFooterHeightObservable(): BehaviorSubject<number> {
+    return this.footerHeight$;
+  }
+
+  setFooterHeight(height: number): void {
+    this.footerHeight$.next(height);
+  }
+
+  getBodyViewHeightPx(): string {
+    return `${window.innerHeight - this.headerHeight$.value - this.footerHeight$.value}px`;
   }
 }
